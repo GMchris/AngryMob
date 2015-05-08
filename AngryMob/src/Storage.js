ba3fbf7e5fae68363300577315f93d1882997258
Kristian Ignatov <christianignatov@gmail.com>
2015-05-08 19:09:55 +0300

    Base safe

---
+++ AngryMob/src/Storage.js
@@ -0,0 +1,20 @@
+// AngryMob Copyright (c) 2015 Todor Radkov and Kristian Ignatov
+
+var Storage = (function() {
+
+  var  memory= {
+
+  };
+
+  function save() {
+
+  }
+
+  function load() {
+
+  }
+
+  return {
+
+  }
+})();
