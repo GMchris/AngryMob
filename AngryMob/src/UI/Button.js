852ad3d6115688c4eaa18249c88d7f655477e927
todorRadkov <todor.radkov@gmail.com>
2015-07-09 09:27:56 +0300

    Changed button vision on active state

--- AngryMob/src/UI/Button.js
+++ AngryMob/src/UI/Button.js
@@ -1,14 +1,29 @@
 // AngryMob Copyright (c) 2015 Todor Radkov and Kristian Ignatov
 
 var Button = cc.MenuItemSprite.extend({
-  ctor: function(labelString, position, callback, image, downImage) {
+  ctor: function(labelString, position, callback, image, downImage, disabledImage) {
+
+    var normalSprite = new cc.Sprite(image || '#common_button.png');
+    var activeSprite =  new cc.Sprite(downImage || image || '#common_button_active.png');
+    var disabledSprite = new cc.Sprite(disabledImage || image || '#common_button_active.png');
+
+    if (image && !downImage) {
+      activeSprite.color = cc.color(100, 100, 100);
+      activeSprite.setScale(0.9);
+      activeSprite.x = activeSprite.width/2 - activeSprite.width*0.9/2;
+      activeSprite.y = activeSprite.height/2 - activeSprite.height*0.9/2;
+    }
 
     this._super(
-      new cc.Sprite(image || '#common_button.png'),
-      new cc.Sprite(downImage || image || '#common_button_active.png'),
-      new cc.Sprite(downImage || image || '#common_button_active.png'),
+        normalSprite,
+        activeSprite,
+        disabledSprite,
       callback
     );
+
+    function getDarkerActiveButton(image){
+
+    }
 
     this.setPosition(position);
     this.labelString = labelString;
